8da3bd6e473c09c43fb97a930a7621c77027080c
jules2pommes <jules.jag.28@gmail.com>
2026-03-18 11:48:52 +0100

    TP2-4 en séance

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -10,10 +10,26 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println(String.format("Je suis désolé %s, mais il faut un habitant de notre village pour commercer ici", nomVendeur));
+		} else {
+			System.out.println(String.format("Bonjour %s, je vais regarder si je peux vous trouver un étal.", nomVendeur));
+			if (!controlPrendreEtal.resteEtals()) 
+				System.out.println(String.format("Désolé %s, je n'ai plus d'étal qui ne soit pas déjà occupé.", nomVendeur));
+			else
+				installerVendeur(nomVendeur);
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait, il me reste un étal pour vous !");
+		System.out.println("Il me faudrait quelques renseignements :");
+		String produit = Clavier.entrerChaine("Quel produit souhaitez vous vendre ?");
+		int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre ?");
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		
+		if (numeroEtal != -1) {
+			System.out.println(String.format("Le vendeur %s s'est installé à l'étal n°%d", nomVendeur, numeroEtal+1));
+		}
 	}
 }
